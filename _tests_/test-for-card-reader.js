384eae60defeedbd552aa83eb38c38b8a0036864
Jason Taisey <jdtaisey89@icloud.com>
2021-08-20 09:37:16 -0700

    Added in the testing files, creating the first test for card reader activation

--- _tests_/test-for-card-reader.js
+++ _tests_/test-for-card-reader.js
@@ -1,2 +1,11 @@
 'use strict'
 
+const ItemScanTest = require('../Source/index');
+
+// Test for Card Reader Activate
+
+describe('Test to see if Card Reader will Activate', () => {
+  test('It should activate the card reader', () => {
+    expect(ItemScanTest)
+  })
+})
