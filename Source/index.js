229d2a8c8c1f9a2882cbef57cd8cb46d5261b5fd
Jason Taisey <jdtaisey89@icloud.com>
2021-08-20 11:16:18 -0700

    Worked through the card cashback ammount, taking a pause to think about payment types

--- Source/index.js
+++ Source/index.js
@@ -27,7 +27,13 @@ function cardInsert () {
     onclick === true;
   } false;
 }
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// NOTE: Card tap or phone tap would need to connect to the cashback
 
+// TODO: Create the conditional payment for when a phone is used
+// TODO: Create the conditional payment for when a card is taped
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 function paymentSelect () {
   let cardInsert = option1
   let cardTap = option2
@@ -37,6 +43,20 @@ function paymentSelect () {
   let red = 'No'
   if(cardInsert || cardTap || phoneTap) { // Created the first edge case to for how the payment is presented
     prompt(green || red) // TODO: Figure out how to display to the DOM so it displays cash back with a yes/no option
+  }
+  if(green) { // Edge Case to display cashback amount
+    // TODO: The cashback amounts will need an if conditional to send to the Cash Register System (CRS)
+    let amount1 = '$20';
+    let amount2 = '$40';
+    let amount3 = '$60';
+    let display = (amount1, amount2, amount3)
+    display();
+  }
+  if(red) { // Edge Case to skip cashback
+    let debt = 'Debt';
+    let credit = 'Credit';
+    let cardType = (debt, credit)
+    cardType();
   }
 }
 
